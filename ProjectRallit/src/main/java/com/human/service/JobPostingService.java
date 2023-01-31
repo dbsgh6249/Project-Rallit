144a74570529e89304e46b5b5deb0db90c3e339e
ceb <ceb5121@gmail.com>
2023-01-31 16:55:02 +0900

    jobPostingService

---
+++ ProjectRallit/src/main/java/com/human/service/JobPostingService.java
@@ -0,0 +1,16 @@
+package com.human.service;
+
+import java.util.List;
+import com.human.dto.JobPostingDto;
+
+public interface ICompanyService {
+	public void create(JobPostingDto dto) throws Exception;
+	public JobPostingDto read(int textNumber) throws Exception;
+	public List<JobPostingDto> readHoldState() throws Exception;
+	public List<JobPostingDto> readDelete() throws Exception;
+	public void delete(int textNumber) throws Exception;
+	public void update(JobPostingDto dto) throws Exception;
+	public JobPostingDto updateHoldState(int textNumber) throws Exception;
+	public JobPostingDto updateDelete(int textNumber) throws Exception;
+	public List<JobPostingDto> listAll() throws Exception;
+}

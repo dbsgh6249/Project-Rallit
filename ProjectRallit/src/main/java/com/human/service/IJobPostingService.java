7617e17c2dc979f5e868277c23946309c7aec019
ceb <ceb5121@gmail.com>
2023-01-31 17:02:02 +0900

    JopPostingService/implement 작성

--- ProjectRallit/src/main/java/com/human/service/IJobPostingService.java
+++ ProjectRallit/src/main/java/com/human/service/IJobPostingService.java
@@ -3,14 +3,14 @@ package com.human.service;
 import java.util.List;
 import com.human.dto.JobPostingDto;
 
-public interface ICompanyService {
-	public void create(JobPostingDto dto) throws Exception;
+public interface IJobPostingService {
+	public void insert(JobPostingDto dto) throws Exception;
 	public JobPostingDto read(int textNumber) throws Exception;
 	public List<JobPostingDto> readHoldState() throws Exception;
 	public List<JobPostingDto> readDelete() throws Exception;
 	public void delete(int textNumber) throws Exception;
 	public void update(JobPostingDto dto) throws Exception;
-	public JobPostingDto updateHoldState(int textNumber) throws Exception;
-	public JobPostingDto updateDelete(int textNumber) throws Exception;
-	public List<JobPostingDto> listAll() throws Exception;
+	public void updateHoldState(int textNumber) throws Exception;
+	public void updateDelete(int textNumber) throws Exception;
+	public List<JobPostingDto> selectAll() throws Exception;
 }

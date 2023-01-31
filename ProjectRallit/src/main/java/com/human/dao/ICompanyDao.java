c540e3b7577bc1e0a260db4126717b6a439ccf0a
471382 <woofer97@naver.com>
2023-01-31 15:39:10 +0900

    companydao와jobseekerdao

--- ProjectRallit/src/main/java/com/human/dao/ICompanyDao.java
+++ ProjectRallit/src/main/java/com/human/dao/ICompanyDao.java
@@ -1,5 +1,9 @@
 package com.human.dao;
 
 public interface ICompanyDao {
-
+	public void insert(CompanyDto dto) throws Exception;
+	public CompanyDto select(String company_id) throws Exception;
+	public List<CompanyDto> selectAll() throws Exception;
+	public void delete(String company_id) throws Exception;
+	public void update(CompanyDto dto) throws Exception;
 }
---
+++ ProjectRallit/src/main/java/com/human/dao/IJobSeekerDao.java
@@ -0,0 +1,10 @@
+package com.human.dao;
+
+public interface IJobSeekerDao {
+	public void insert(JobSeekerDto dto) throws Exception;
+	public JobSeekerDto select(String seeker_id) throws Exception;
+	public List<JobSeekerDto> selectAll() throws Exception;
+	public void delete(String seeker_id) throws Exception;
+	public void update(JobSeekerDto dto) throws Exception;
+}
+}

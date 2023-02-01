0113f44e5376b4f0801fa2070263aeba86c05531
ceb <ceb5121@gmail.com>
2023-02-01 16:53:12 +0900

    jobPostingDao 작성, 기존 파일 오타 수정

--- ProjectRallit/src/main/java/com/human/dao/H_UsersDao.java
+++ ProjectRallit/src/main/java/com/human/dao/H_UsersDao.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import com.human.dto.H_UsersDto;
 
-public interface H_UsersDto {
+public interface H_UsersDao {
 	public void insert(H_UsersDto dto) throws Exception;
 	public H_UsersDto selectOne(String user_id) throws Exception;
-	public List<AuthoritiesDto> selectAll() throws Exception;
+	public List<H_UsersDto> selectAll() throws Exception;
 	public void delete(String user_id) throws Exception;
 	public void update(H_UsersDto dto) throws Exception;
 }

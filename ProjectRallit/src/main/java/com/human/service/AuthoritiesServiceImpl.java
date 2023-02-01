7b03a761666ce4f644644d26515f5e7189cfc6b5
ceb <ceb5121@gmail.com>
2023-02-01 17:27:24 +0900

    권한,채용공고 서비스 구현부 작성

---
+++ ProjectRallit/src/main/java/com/human/service/AuthoritiesServiceImpl.java
@@ -0,0 +1,53 @@
+package com.human.service;
+
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.human.dto.AuthoritiesDto;
+import com.human.dao.IAuthoritiesDao;
+
+@Service
+public class AuthoritiesServiceImpl implements IAuthoritiesService{
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public void insertSeeker(AuthoritiesDto dto) throws Exception {
+		IAuthoritiesDao dao = sqlSession.getMapper(IAuthoritiesDao.class);
+		dao.insertSeeker(dto);
+	}
+
+	@Override
+	public void insertCompany(AuthoritiesDto dto) throws Exception {
+		IAuthoritiesDao dao = sqlSession.getMapper(IAuthoritiesDao.class);
+		dao.insertCompany(dto);
+	}
+
+	@Override
+	public List<AuthoritiesDto> selectAll() throws Exception {
+		IAuthoritiesDao dao = sqlSession.getMapper(IAuthoritiesDao.class);
+		return dao.selectAll();
+	}
+
+	@Override
+	public List<AuthoritiesDto> selectSeeker() throws Exception {
+		IAuthoritiesDao dao = sqlSession.getMapper(IAuthoritiesDao.class);
+		return dao.selectSeeker();
+	}
+
+	@Override
+	public List<AuthoritiesDto> selectCompany() throws Exception {
+		IAuthoritiesDao dao = sqlSession.getMapper(IAuthoritiesDao.class);
+		return dao.selectCompany();
+	}
+
+	@Override
+	public void delete(String user_id, String authority) throws Exception {
+		IAuthoritiesDao dao = sqlSession.getMapper(IAuthoritiesDao.class);
+		dao.delete(user_id, authority);
+	}
+
+}
---
+++ ProjectRallit/src/main/java/com/human/service/JobPostingServiceImpl.java
@@ -0,0 +1,53 @@
+package com.human.service;
+
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.human.dto.JobPostingDto;
+import com.human.dao.IJobPostingDao;
+
+@Service
+public class JobPostingServiceImpl implements IJobPostingService{
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public void insert(JobPostingDto dto) throws Exception {
+		IJobPostingDao dao = sqlSession.getMapper(IJobPostingDao.class);
+		dao.insert(dto);
+	}
+
+	@Override
+	public JobPostingDto selectOne(String posting_num) throws Exception {
+		IJobPostingDao dao = sqlSession.getMapper(IJobPostingDao.class);
+		return dao.selectOne(posting_num);
+	}
+
+	@Override
+	public List<JobPostingDto> selectCompanyPosting() throws Exception {
+		IJobPostingDao dao = sqlSession.getMapper(IJobPostingDao.class);
+		return dao.selectCompanyPosting();
+	}
+
+	@Override
+	public List<JobPostingDto> selectAll() throws Exception {
+		IJobPostingDao dao = sqlSession.getMapper(IJobPostingDao.class);
+		return dao.selectAll();
+	}
+
+	@Override
+	public void delete(String posting_num) throws Exception {
+		IJobPostingDao dao = sqlSession.getMapper(IJobPostingDao.class);
+		dao.delete(posting_num);
+	}
+
+	@Override
+	public void update(JobPostingDto dto) throws Exception {
+		IJobPostingDao dao = sqlSession.getMapper(IJobPostingDao.class);
+		dao.update(dto);
+	}
+
+}

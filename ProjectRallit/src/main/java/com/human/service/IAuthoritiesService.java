9110e395801bc2456017c117ba9c8217ed1b1a79
471382 <woofer97@naver.com>
2023-02-01 17:18:54 +0900

    authorities,jobposting,postingtagservice commit

---
+++ ProjectRallit/src/main/java/com/human/service/IAuthoritiesService.java
@@ -0,0 +1,14 @@
+package com.human.service;
+
+import java.util.List;
+
+import com.human.dto.AuthoritiesDto;
+
+public interface IAuthoritiesService {
+	public void insertSeeker(AuthoritiesDto dto) throws Exception;
+	public void insertCompany(AuthoritiesDto dto) throws Exception;
+	public List<AuthoritiesDto> selectAll() throws Exception;
+	public List<AuthoritiesDto> selectSeeker() throws Exception;
+	public List<AuthoritiesDto> selectCompany() throws Exception;
+	public void delete(String user_id, String authority) throws Exception;
+}
---
+++ ProjectRallit/src/main/java/com/human/service/IPostingTagService.java
@@ -0,0 +1,12 @@
+package com.human.service;
+
+import java.util.List;
+import com.human.dto.PostingTagDto;
+
+public interface IPostingTagService {
+	public void insert(PostingTagDto dto) throws Exception;
+	public List<PostingTagDto> select(int posting_num) throws Exception;
+	public void delete(int posting_num) throws Exception;
+	public void deleteTag(int posting_num,String languageTag) throws Exception;
+	public void update(PostingTagDto dto) throws Exception;
+}

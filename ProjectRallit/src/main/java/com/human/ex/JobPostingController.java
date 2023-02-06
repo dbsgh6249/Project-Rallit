ab429b2ea4c097e0ed9ed30b4296a51068d0f658
yujin <pwls5243@naver.com>
2023-02-06 14:34:58 +0900

    commit

--- ProjectRallit/src/main/java/com/human/ex/JobPostingController.java
+++ ProjectRallit/src/main/java/com/human/ex/JobPostingController.java
@@ -39,14 +39,12 @@ public class JobPostingController {
 	private IPostingTagService postingTag_Service;
 	@Autowired
 	private IJobPostingInfoService jobPostingInfo_Service;
-<<<<<<< HEAD
+
 	
 	/*
 	 * @Resource(name = "uploadPath") private String uploadPath;
 	 */
-=======
 
->>>>>>> branch 'master' of https://github.com/dbsgh6249/Project-Rallit.git
 	
 	@RequestMapping(value = "/jobPosting/insert", method = RequestMethod.GET)
 	public String jobPostingInsert() {
